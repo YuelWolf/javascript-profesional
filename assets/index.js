400e5f7bc3e2120a53a2d8088d1500c25db311f8
Andrés Rodríguez <darkknight64@gmail.com>
2020-05-21 11:25:24 -0500

    Added examples for global, function and block scope

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,8 @@
+import MediaPlayer from './MediaPlayer.js';
+
 const video = document.querySelector("video")
 const buttonPlay = document.querySelector("#playButton")
 const buttonMute = document.querySelector("#muteButton")
-
-function MediaPlayer(config) {
-    this.media = config.el
-}
-
-MediaPlayer.prototype.play = function () {
-    (this.media.paused) ? this.media.play() : this.media.pause()
-}
-
-MediaPlayer.prototype.mute = function () {
-    (this.media.muted) ? this.media.muted = false : this.media.muted = true;
-
-}
 
 const player = new MediaPlayer({el: video});
 
