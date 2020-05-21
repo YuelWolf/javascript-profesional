dbaa4b9e9614b8928871a4d2ac37d6d7d324b8b2
Andrés Rodríguez <darkknight64@gmail.com>
2020-05-21 12:39:42 -0500

    Added this examples

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,9 +1,18 @@
 function MediaPlayer(config) {
-    this.media = config.el
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function () {
+    this.plugins.forEach(plugin => {
+        plugin.run(this)
+    });
 }
 
 MediaPlayer.prototype.play = function () {
-    (this.media.paused) ? this.media.play() : this.media.pause()
+    (this.media.paused) ? this.media.play() : this.media.pause();
 }
 
 MediaPlayer.prototype.mute = function () {
--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,14 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector("video")
 const buttonPlay = document.querySelector("#playButton")
 const buttonMute = document.querySelector("#muteButton")
 
-const player = new MediaPlayer({el: video});
+const player = new MediaPlayer({el: video, plugins: [
+    //new AutoPlay()
+
+]});
 
 buttonPlay.onclick = () => player.play();
 buttonMute.onclick = () => player.mute();
